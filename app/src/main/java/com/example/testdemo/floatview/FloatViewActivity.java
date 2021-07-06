9e5f730b40d93b32322d07d46d91e8fcb967d465
oukanggui <13802885143@139.com>
2021-07-06 17:09:29 +0800

    添加悬浮窗功能逻辑

--- app/src/main/java/com/example/testdemo/floatview/FloatViewActivity.java
+++ app/src/main/java/com/example/testdemo/floatview/FloatViewActivity.java
@@ -1,15 +1,95 @@
 package com.example.testdemo.floatview;
 
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.testdemo.R;
 
 public class FloatViewActivity extends AppCompatActivity {
+    private FloatingWindowHelper floatingWindowHelper;
+    private Button btnShow, btnHide;
+    private MusicFloatLayout musicFloatLayout;
+    private View floatWindowDeleteView;
+    private TextView tvDelete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_float_view);
+        FloatingWindowHelper.canDrawOverlays(this, true);
+        floatingWindowHelper = new FloatingWindowHelper(this);
+        View musicRootLayout = LayoutInflater.from(this).inflate(R.layout.layout_float_window, null, false);
+        musicFloatLayout = musicRootLayout.findViewById(R.id.layout_root);
+        floatWindowDeleteView = LayoutInflater.from(this).inflate(R.layout.layout_float_window_delete, null, false);
+        tvDelete = floatWindowDeleteView.findViewById(R.id.tv_delete);
+        btnShow = findViewById(R.id.btn_show);
+        btnHide = findViewById(R.id.btn_hide);
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                musicFloatLayout.show(new MusicFloatLayout.OnDragListener() {
+                    @Override
+                    public void onDragStart() {
+
+                    }
+
+                    @Override
+                    public void onDragging(boolean isReachDeleteArea) {
+                        Log.d("CBaymax", "onDragging:" + isReachDeleteArea);
+                        floatingWindowHelper.addView(floatWindowDeleteView, createDeleteLayoutParams());
+                        if (tvDelete != null) {
+                            tvDelete.setText(isReachDeleteArea ? "放开手删除" : "拖拽到此处删除");
+                        }
+                    }
+
+                    @Override
+                    public void onDragEnd(boolean isReachDeleteArea) {
+                        Log.d("CBaymax", "onDragEnd:" + isReachDeleteArea);
+                        floatingWindowHelper.removeView(floatWindowDeleteView);
+                    }
+                });
+            }
+        });
+        btnHide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                musicFloatLayout.dismiss();
+            }
+        });
     }
+
+    /**
+     * 创建删除布局的LayoutParams
+     */
+    private WindowManager.LayoutParams createDeleteLayoutParams() {
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            layoutParams.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+        } else {
+            layoutParams.type = WindowManager.LayoutParams.TYPE_PHONE;
+        }
+        layoutParams.format = PixelFormat.RGBA_8888;
+        layoutParams.gravity = Gravity.BOTTOM;
+        layoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+        layoutParams.width = LinearLayout.LayoutParams.MATCH_PARENT;
+        layoutParams.height = dp2px(this, 60);
+        return layoutParams;
+    }
+
+    private int dp2px(Context context, float dip) {
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dip * scale + 0.5F * (float) (dip >= 0.0F ? 1 : -1));
+    }
+
 }
---
+++ app/src/main/java/com/example/testdemo/floatview/MusicFloatLayout.java
@@ -0,0 +1,228 @@
+package com.example.testdemo.floatview;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+
+import static android.content.Context.WINDOW_SERVICE;
+
+/**
+ * @author oukanggui
+ * @date 2021/7/6
+ * @description 音乐悬浮窗布局
+ */
+public class MusicFloatLayout extends FrameLayout {
+    /**
+     * 窗口宽高
+     */
+    private int mWindowWidthPixels, mWindowHeightPixels;
+    /**
+     * 悬浮窗管理相关
+     */
+    private WindowManager mWindowManager;
+    private WindowManager.LayoutParams mLayoutParams;
+    private boolean mIsShow;
+    private int mDeleteViewHeight = 0;
+    private int x;
+    private int y;
+
+    private OnDragListener onDragListener;
+
+    /**
+     * 拖拽监听器
+     */
+    public interface OnDragListener {
+        /**
+         * 开始拖拽
+         */
+        void onDragStart();
+
+        /**
+         * 拖拽中
+         *
+         * @param isReachDeleteArea 是否到达删除区域
+         */
+        void onDragging(boolean isReachDeleteArea);
+
+        /**
+         * 拖拽结束
+         *
+         * @param isReachDeleteArea 是否到达删除区域
+         */
+        void onDragEnd(boolean isReachDeleteArea);
+    }
+
+    public MusicFloatLayout(@NonNull Context context) {
+        super(context);
+        init(context);
+    }
+
+    public MusicFloatLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public MusicFloatLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public MusicFloatLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+    /**
+     * 处理触摸事件，实现拖动、形状变更和粘边效果
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (mWindowManager != null) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    x = (int) event.getRawX();
+                    y = (int) event.getRawY();
+                    if (onDragListener != null) {
+                        onDragListener.onDragStart();
+                    }
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    int nowX = (int) event.getRawX();
+                    int nowY = (int) event.getRawY();
+                    int movedX = nowX - x;
+                    int movedY = nowY - y;
+                    x = nowX;
+                    y = nowY;
+                    mLayoutParams.x = mLayoutParams.x + movedX;
+                    mLayoutParams.y = mLayoutParams.y + movedY;
+                    if (mLayoutParams.x < 0) {
+                        mLayoutParams.x = 0;
+                    }
+                    if (mLayoutParams.y < 0) {
+                        mLayoutParams.y = 0;
+                    }
+                    mWindowManager.updateViewLayout(this, mLayoutParams);
+                    if (onDragListener != null) {
+                        onDragListener.onDragging(isDragViewReachDeleteArea());
+                    }
+                    break;
+                case MotionEvent.ACTION_UP:
+                    boolean isReachDeleteArea = isDragViewReachDeleteArea();
+                    if (onDragListener != null) {
+                        onDragListener.onDragEnd(isReachDeleteArea);
+                    }
+                    if (!isReachDeleteArea) {
+                        // 未到达删除区域
+                        alignToWindowRight((int) event.getRawX(), (int) event.getRawY());
+                        invalidate();
+                        mWindowManager.updateViewLayout(this, mLayoutParams);
+                    } else {
+                        // 已达到删除区域
+                        dismiss();
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        return super.onTouchEvent(event);
+    }
+
+    /**
+     * @param context
+     */
+    private void init(Context context) {
+        //悬浮窗管理相关
+        mWindowManager = (WindowManager) getContext().getSystemService(WINDOW_SERVICE);
+        mLayoutParams = new WindowManager.LayoutParams();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            mLayoutParams.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+        } else {
+            mLayoutParams.type = WindowManager.LayoutParams.TYPE_PHONE;
+        }
+        mLayoutParams.format = PixelFormat.RGBA_8888;
+        mLayoutParams.gravity = Gravity.START | Gravity.TOP;
+        mLayoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+        mLayoutParams.width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        mLayoutParams.height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        mWindowManager.getDefaultDisplay().getMetrics(outMetrics);
+        mWindowWidthPixels = outMetrics.widthPixels;
+        mWindowHeightPixels = outMetrics.heightPixels;
+        mDeleteViewHeight = dp2px(context, 60);
+    }
+
+    private int dp2px(Context context, float dip) {
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dip * scale + 0.5F * (float) (dip >= 0.0F ? 1 : -1));
+    }
+
+    /**
+     * 判断拖拽布局是否到达删除区域
+     *
+     * @return
+     */
+    private boolean isDragViewReachDeleteArea() {
+        if ((mLayoutParams.y + getMeasuredHeight()) >= (mWindowHeightPixels - mDeleteViewHeight)) {
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * 判定所处方向
+     */
+    private void alignToWindowRight(int x, int y) {
+        mLayoutParams.x = mWindowWidthPixels - getMeasuredWidth();
+    }
+
+
+    /**
+     * 显示悬浮窗
+     * 一开始默认靠右居中位置
+     */
+    public void show(OnDragListener onDragListener) {
+        this.onDragListener = onDragListener;
+        if (!mIsShow) {
+            mLayoutParams.x = mWindowWidthPixels - getMeasuredWidth();
+            mLayoutParams.y = mWindowHeightPixels / 2 - getMeasuredHeight() / 2;
+            mWindowManager.addView(this, mLayoutParams);
+            mIsShow = true;
+        }
+    }
+
+    /**
+     * dismiss
+     */
+    public void dismiss() {
+        if (mIsShow) {
+            mWindowManager.removeView(this);
+            onDragListener = null;
+            mIsShow = false;
+        }
+    }
+
+
+}
